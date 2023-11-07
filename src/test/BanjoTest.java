b9f9807e535f7571161be176c8498f0d11d35c0d
mohsenre89 <mohsen.rezaei89@yahoo.com>
2023-11-07 22:55:44 +0100

    Added .idea to gitignore, refactored BanjoTest

--- src/test/BanjoTest.java
+++ src/test/BanjoTest.java
@@ -5,15 +5,29 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Assertions;
 
 public class BanjoTest {
+
     @Test
-    public void isPlayingBanjo_whenNameStartsWithR_thenTrue() {
+    public void testPlayingBanjo() {
         String startsWithR = Banjo.areYouPlayingBanjo("Rick");
         Assertions.assertEquals(startsWithR, "Rick plays banjo");
     }
 
     @Test
-    public void isPlayingBanjo_whenNameNotStartsWithR_thenFalse() {
+    public void testNotPlayingBanjo() {
         String startsWithR = Banjo.areYouPlayingBanjo("Ben");
         Assertions.assertEquals(startsWithR, "Ben does not play banjo");
     }
+
+    @Test
+    public void testPlayingBanjoCaseInsensitive() {
+        String result = Banjo.areYouPlayingBanjo("rachel");
+        Assertions.assertEquals("rachel plays banjo", result);
+    }
+
+    @Test
+    public void testNotPlayingBanjoEmptyString() {
+        String result = Banjo.areYouPlayingBanjo("");
+        Assertions.assertEquals(" does not play banjo", result); // Empty name should return " does not play banjo"
+    }
+
 }
