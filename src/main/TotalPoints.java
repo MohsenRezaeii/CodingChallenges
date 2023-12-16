7a928ed5651b5cc8ffe450041b2e75c7dfa9b9c1
mohsenre89 <mohsen.rezaei89@yahoo.com>
2023-12-16 09:57:47 +0100

    Added TotalPoints and its unit tests

---
+++ src/main/TotalPoints.java
@@ -0,0 +1,18 @@
+package main;
+
+public class TotalPoints {
+    public static int points(String[] games) {
+        int ourScore = 0;
+        for (String game : games) {
+            char ourTeamScore = game.charAt(0);
+            char opponentScore = game.charAt(2);
+
+            if (ourTeamScore > opponentScore) {
+                ourScore += 3;
+            } else if (ourTeamScore == opponentScore) {
+                ourScore++;
+            }
+        }
+        return ourScore;
+    }
+}
---
+++ src/test/TotalPointsTest.java
@@ -0,0 +1,38 @@
+package test;
+
+import main.TotalPoints;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TotalPointsTest {
+    @Test
+    public void testPointsForWin() {
+        String[] games = {"3:0", "2:1", "4:2"};
+        assertEquals(9, TotalPoints.points(games));
+    }
+
+    @Test
+    public void testPointsForLoss() {
+        String[] games = {"1:2", "0:3", "2:4"};
+        assertEquals(0, TotalPoints.points(games));
+    }
+
+    @Test
+    public void testPointsForDraw() {
+        String[] games = {"2:2", "1:1", "3:3"};
+        assertEquals(3, TotalPoints.points(games));
+    }
+
+    @Test
+    public void testPointsWithMixedResults() {
+        String[] games = {"2:1", "0:3", "1:1", "3:0", "2:2"};
+        assertEquals(8, TotalPoints.points(games));
+    }
+
+    @Test
+    public void testPointsWithEmptyArray() {
+        String[] games = {};
+        assertEquals(0, TotalPoints.points(games));
+    }
+}
